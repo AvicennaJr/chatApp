0384c093826e55a7561a2791643fb79c90437e23
AvicennaJr <justfuad@protonmail.com>
2023-01-24 19:19:28 +0300

    Handle events and concurrency through unbuffered channels

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 
 	"github.com/gorilla/websocket"
@@ -13,12 +14,15 @@ type ClientList map[*Client]bool
 type Client struct {
 	connection *websocket.Conn
 	manager    *Manager
+	// egress is used to avoid concurrent writes on the websocket connection
+	egress chan Event
 }
 
 func NewClient(conn *websocket.Conn, manager *Manager) *Client {
 	return &Client{
 		connection: conn,
 		manager:    manager, // will help to broadcast to other clients
+		egress:     make(chan Event),
 	}
 }
 
@@ -30,7 +34,7 @@ func (c *Client) readMessages() {
 	}()
 
 	for {
-		messageType, payLoad, err := c.connection.ReadMessage()
+		_, payLoad, err := c.connection.ReadMessage()
 		if err != nil {
 			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
 				log.Printf("error reading message: %v", err)
@@ -38,7 +42,45 @@ func (c *Client) readMessages() {
 			break
 		}
 
-		log.Println(messageType)
-		log.Println(string(payLoad))
+		var request Event
+
+		if err := json.Unmarshal(payLoad, &request); err != nil {
+			log.Println("Error marshling event: ", err)
+			break
+		}
+
+		if err := c.manager.routeEvent(request, c); err != nil {
+			log.Println("error handling message: ", err)
+		}
+	}
+}
+
+func (c *Client) writeMessages() {
+	defer func() {
+		c.manager.removeClient(c)
+	}()
+
+	for {
+		select {
+		case message, ok := <-c.egress:
+			if !ok {
+				if err := c.connection.WriteMessage(websocket.CloseMessage, nil); err != nil {
+					log.Println("connection closed: ", err)
+				}
+				return
+			}
+
+			data, err := json.Marshal(message)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			if err := c.connection.WriteMessage(websocket.TextMessage, data); err != nil {
+				log.Println("failed to send message: ", err)
+			}
+
+			log.Println("message sent")
+		}
 	}
 }
