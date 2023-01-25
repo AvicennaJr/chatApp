885b4fe9f3937b449e531fb6836ef6b253a076de
AvicennaJr <justfuad@protonmail.com>
2023-01-25 23:43:54 +0300

    Add TLS to main

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	setupAPI()
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServeTLS(":8080", "server.crt", "server.key", nil))
 }
 
 func setupAPI() {
