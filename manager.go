426b70017a4a2780f009c4cac7045993a770fff6
AvicennaJr <justfuad@protonmail.com>
2023-01-25 23:43:41 +0300

    Add https to allowed cors

--- manager.go
+++ manager.go
@@ -147,7 +147,7 @@ func checkOrigin(r *http.Request) bool {
 	origin := r.Header.Get("Origin")
 
 	switch origin {
-	case "http://localhost:8080":
+	case "https://localhost:8080":
 		return true
 	default:
 		return false
